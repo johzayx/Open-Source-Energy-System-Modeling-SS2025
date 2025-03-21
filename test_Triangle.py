bd796940ee2b6aee8b1d2b5287cea40fc621f25b
johzayx <e12002236@student.tuwien.ac.at>
2025-03-21 14:11:11 +0100

    Make tests pass
    Before the tests were written to fail, now they go are supposed to pass

--- test_Triangle.py
+++ test_Triangle.py
@@ -2,16 +2,16 @@ from Triangle import angle_Alpha, side_b, side_c, area
 
 
 def test_angle_Alpha():
-    assert angle_Alpha(30, 80) == 60
+    assert angle_Alpha(30, 80) == 70
 
 
 def test_side_b():
-    assert side_b(3, 30, 80) == 3
+    assert side_b(3, 30, 80) == 5.91
 
 
 def test_side_c():
-    assert side_c(3, 30, 80) == 2
+    assert side_c(3, 30, 80) == 5.91
 
 
 def test_area():
-    assert area(3, 5, 60) == 7
+    assert area(3, 5, 60) == 6.5
