28fe5e1a459dd97ff1b293a2f0a1923b744878fe
johzayx <e12002236@student.tuwien.ac.at>
2025-03-21 12:41:32 +0100

    Add test_Triangle
    which includes unit tests

---
+++ test_Triangle.py
@@ -0,0 +1,13 @@
+from Triangle import angle_Alpha, side_b, side_c, area
+
+def test_angle_Alpha():
+    assert angle_Alpha(30, 80) == 60
+
+def test_side_b():
+    assert side_b(3, 30, 80) == 3
+
+def test_side_c():
+    assert side_c(3, 30, 80) == 2
+
+def test_area():
+    assert area(3, 5, 60) == 7
