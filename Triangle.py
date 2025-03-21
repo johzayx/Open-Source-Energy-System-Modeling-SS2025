ecc30e6ee62e5eb419f50708c8498c6e818b0bb7
johzayx <e12002236@student.tuwien.ac.at>
2025-03-21 14:10:10 +0100

    Round Results

--- Triangle.py
+++ Triangle.py
@@ -3,23 +3,23 @@ import math
 
 # Calculate the third angle of the triangle
 def angle_Alpha(Beta, Gamma):
-    alpha = 180 - Beta - Gamma
+    alpha = round(180 - Beta - Gamma, 2)
     return alpha
 
 
 # Calculate side b of the tringle
 def side_b(a, Alpha, Beta):
-    b = a * math.sin(math.radians(Beta)) / math.sin(math.radians(Alpha))
+    b = round(a * math.sin(math.radians(Beta)) / math.sin(math.radians(Alpha)), 2)
     return b
 
 
 # Calculate side c of the triangle
 def side_c(a, Alpha, Gamma):
-    c = a * math.sin(math.radians(Gamma)) / math.sin(math.radians(Alpha))
+    c = round(a * math.sin(math.radians(Gamma)) / math.sin(math.radians(Alpha)), 2)
     return c
 
 
 # Calculate area of the triangle
 def area(a, b, Gamma):
-    area_F = 0.5 * a * b * math.sin(math.radians(Gamma))
+    area_F = round(0.5 * a * b * math.sin(math.radians(Gamma)), 2)
     return area_F
