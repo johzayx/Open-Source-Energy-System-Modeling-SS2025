f219a624dcd4cf49fcd02fee673ae143b4aef3ee
johzayx <e12002236@student.tuwien.ac.at>
2025-03-21 12:40:46 +0100

    Change FUNCTION NAMES
    to make them more descriptive

--- Triangle.py
+++ Triangle.py
@@ -1,21 +1,21 @@
 import math
 
 #Calculate the third angle of the triangle
-def W(Beta, Gamma):
+def angle_Alpha(Beta, Gamma):
     alpha=180 - Beta - Gamma
     return alpha
 
 #Calculate side b of the tringle
-def b(a, Alpha, Beta):
+def side_b(a, Alpha, Beta):
     b=a * math.sin(math.radians(Beta)) / math.sin(math.radians(Alpha))
     return b
 
 #Calculate side c of the triangle
-def c(a, Alpha, Gamma):
+def side_c(a, Alpha, Gamma):
     c=a * math.sin(math.radians(Gamma)) / math.sin(math.radians(Alpha))
     return c
 
 #Calculate area of the triangle
-def F(a, b, Gamma):
+def area(a, b, Gamma):
     area_F = 0.5 * a * b * math.sin(math.radians(Gamma))
     return area_F
